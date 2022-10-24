f142caca446c4389d63d780d74956376500cdf24
Alumno <ulsa@D105.local>
2022-10-24 08:48:01 -0700

    1era

---
+++ Practica-contactos/CeldaContactoController.swift
@@ -0,0 +1,16 @@
+//
+//  CeldaContactoController.swift
+//  Practica-contactos
+//
+//  Created by Alumno on 24/10/22.
+//  Copyright © 2022 Sebastian Encinas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class CeldaContactoController: UITableViewCell {
+    
+    @IBOutlet weak var lblNombre: UILabel!
+    
+    @IBOutlet weak var lblNumero: UILabel!
+}
---
+++ Practica-contactos/contactos.swift
@@ -0,0 +1,18 @@
+//
+//  contactos.swift
+//  Practica-contactos
+//
+//  Created by Alumno on 24/10/22.
+//  Copyright © 2022 Sebastian Encinas. All rights reserved.
+//
+
+class contactos {
+    var nombre: String
+    var numero: String
+    
+    init (nombre:String, numero:String){
+        self.nombre = nombre
+        self.numero = numero
+    
+}
+}
